1c6c6a02ccf0fb4d9e23b545a8842bc9f5bb61e6
Dzhelil Rufat <drufat@caltech.edu>
2013-04-17 23:05:25 -0700

    Update documents.

--- bases-1.py
+++
@@ -1,6 +0,0 @@
-from dec.spectral import *
-from matplotlib.pyplot import *
-from dec.plot import plot_bases_1d
-N = 5
-g = Grid_1D_Periodic(N)
-plot_bases_1d(g, g.xmin, g.xmax, "\phi")
--- bases-3.py
+++
@@ -1,7 +0,0 @@
-from dec.spectral import *
-from matplotlib.pyplot import *
-from dec.plot import plot_bases_1d
-N = 5
-g = Grid_1D_Chebyshev(N)
-plot_bases_1d(g, g.xmin, g.xmax, "\kappa")
-show()
