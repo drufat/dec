3ae1801b86a6573a0c0736216504d2415d5735a6
Dzhelil Rufat <drufat@caltech.edu>
2014-12-29 01:04:36 -0800

    Update plotting.

--- doc/plot/cheb/resample.py
+++ doc/plot/cheb/resample.py
@@ -1,5 +1,5 @@
 from dec.spectral import *
-from matplotlib.pyplot import *
+import matplotlib.pyplot as plt
 
 N = 5
 g = Grid_1D_Periodic(N)
@@ -9,22 +9,22 @@ random.seed(seed=1)
 y = random.rand(N)
 
 x = linspace(0, 2*pi, 100)
-plot(x, R0(y)(x)  , color='k')
-scatter(g.verts, y, marker='o', color='k')
+plt.plot(x, R0(y)(x)  , color='k')
+plt.scatter(g.verts, y, marker='o', color='k')
 
 n = 2
 g2 = Grid_1D_Periodic(N+2*n)
 y2 = real( Finv(extend(F(y), n)) )
-plot(x, R0(y)(x)+1, color='k')
-scatter(g2.verts, y2+1, marker='o', color='k')
+plt.plot(x, R0(y)(x)+1.0, color='k')
+plt.scatter(g2.verts, y2+1.0, marker='o', color='k')
 
 #n = 2
 #g3 = Grid_1D_Periodic(N-2*n)
 #y3 = real( Finv(unextend(F(y), n)) )
 #scatter(g3.verts, y3, marker='x', color='g')
 
-frame = gca()
+frame = plt.gca()
 frame.axes.get_xaxis().set_visible(False)
 frame.axes.get_yaxis().set_visible(False)
 
-show()
+plt.show()
