706a3811bc504e1401e866b28bcf7a97242abd03
Dzhelil Rufat <drufat@caltech.edu>
2015-02-25 16:07:30 -0800

    Split tests.

--- dec/grid1/test/all_test.py
+++ dec/grid1/test/all_test.py
@@ -1,9 +1,8 @@
 from dec.spectral import *
 from dec.grid1 import *
-from numpy.testing import assert_array_almost_equal
+from numpy.testing import assert_array_almost_equal as eq
 import dec.forms
 
-eq = assert_array_almost_equal
 
 # def test_DEC():
 #     g = Grid_1D_Periodic(11)
@@ -306,46 +305,3 @@ def test_associativity_old():
 
     eq(eq1, eq2)
     eq(eq2, eq3)
-
-def test_contraction_periodic():
-
-    g = Grid_1D_Periodic(14)
-    P0, P1, P0d, P1d = g.projection()
-
-    def v(x): return sin(4 * x)
-    def a(x): return cos(4 * x)
-    def b(x): return a(x)*v(x) 
-
-    V = P1(v)
-    alpha1 = P1(a)
-    beta0 = P0(b)
-
-    C1 = g.contraction(V)
-    eq(C1(alpha1), beta0)
-
-def test_contraction_chebyshev():
- 
-    g = Grid_1D_Chebyshev(14)
-    P0, P1, P0d, P1d = g.projection()
- 
-    def v(x): return x**2
-    def a(x): return 2*x
-    def b(x): return a(x)*v(x) 
- 
-    V = P1(v)
-    alpha1 = P1(a)
-    beta0 = P0(b)
- 
-    C1 = g.contraction(V)
-    eq(C1(alpha1), beta0)
-
-    def v(x): return x**2
-    def a(x): return x**6
-    def b(x): return a(x)*v(x) 
- 
-    V = P1(v)
-    alpha1 = P1(a)
-    beta0 = P0(b)
- 
-    C1 = g.contraction(V)
-    eq(C1(alpha1), beta0)
