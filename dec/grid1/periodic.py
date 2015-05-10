a8da7c31fc3a24690f5d847fdd77a4a42a7ee747
Dzhelil Rufat <drufat@caltech.edu>
2015-05-09 22:37:13 -0700

    Update periodic.

--- dec/grid1/periodic.py
+++ dec/grid1/periodic.py
@@ -1,7 +1,96 @@
 import numpy as np
 import dec.spectral as sp
+from dec.grid1 import projections, dec_operators
+from collections import namedtuple
 
-class Grid_1D_Periodic_(object):
+dec_operators = namedtuple('dec_operators', 'P B D H')
+
+def periodic_fn(cls, n, xmin=0, xmax=2*np.pi):
+
+    pnts = np.linspace(xmin, xmax, num=(n+1))
+    lenx = abs(xmax - xmin)
+    delta = np.diff(pnts)
+
+    verts = pnts[:-1]
+    edges = (pnts[:-1], pnts[1:])
+
+    verts_dual = verts + 0.5*delta
+    edges_dual = (np.roll(verts_dual,shift=1), verts_dual)
+    edges_dual[0][0] -= lenx
+    delta_dual = delta
+
+    B0  = lambda i, x: sp.phi0(n, i, x)
+    B1  = lambda i, x: sp.phi1(n, i, x)
+    B0d = lambda i, x: sp.phid0(n, i, x)
+    B1d = lambda i, x: sp.phid1(n, i, x)
+    
+    P0,  P1  = projections((verts, edges))
+    P0d, P1d = projections((verts_dual, edges_dual))
+    
+    D0  = lambda f: np.roll(f, shift=-1) - f
+    D1  = lambda f: 0 
+    D0d = lambda f: np.roll(D0(f), shift=+1)
+    D1d = lambda f: 0 
+
+    H0 = lambda x: np.real(sp.H(x))
+    H1 = lambda x: np.real(sp.Hinv(x))
+    H0d = H0
+    H1d = H1
+    
+    d = cls(n=n,
+            xmin=xmin,
+            xmax=xmax,
+            pnts=pnts,
+            delta=delta_dual, 
+            N = (n, n),
+            simp=(verts_dual, edges_dual),
+            dec=dec_operators(P=(P0d, P1d),
+                                 B=(B0d, B1d),
+                                 D=(D0d, D1d),
+                                 H=(H0d, H1d)),
+            dual=None)
+    g = cls(n=n,
+            xmin=xmin, 
+            xmax=xmax,
+            pnts=pnts, 
+            delta=delta,
+            N = (n, n),
+            simp=(verts, edges),
+            dec=dec_operators(P=(P0, P1),
+                                 B=(B0, B1),
+                                 D=(D0, D1),
+                                 H=(H0, H1)),
+            dual=None)  
+    g.dual, d.dual = d, g
+    return g
+
+def projections(simp):
+
+    def P0(f):
+        return f(simp[0])
+    
+    def P1(f):
+        return sp.slow_integration(simp[1][0], simp[1][1], f)
+        #return sp.split_args(sp.integrate_spectral)(grid.simp[1][0], grid.simp[1][1])
+        
+    return P0, P1
+
+def derivatives(grid):
+    D0 = grid.dec.D[0]
+    D0d = grid.dual.dec.D[0]
+    return D0, D0d
+
+def hodge_star(grid):
+    H0 = grid.dec.H[0]
+    H1 = grid.dec.H[1]
+    H0d = grid.dual.dec.H[0]
+    H1d = grid.dual.dec.H[1]
+    return H0, H1, H0d, H1d
+
+class Grid_1D(object):
+    '''
+    >>> g = Grid_1D.periodic(5)
+    '''
     
     def __init__(self, n, xmin, xmax, pnts, delta, N, simp, dec, dual):
         self.dimension = 1
@@ -16,71 +105,9 @@ class Grid_1D_Periodic_(object):
         self.dec = dec
         
     def __repr__(self):
-        return 'Grid_1D_Periodic_.make{}'.format((self.n, self.xmin, self.xmax))
+        return 'Grid_1D{}'.format((self.n, self.xmin, self.xmax))
     
-    @classmethod
-    def make(cls, n, xmin=0, xmax=2*np.pi):
-        import dec.grid1.common as cm #mutual imports?
-    
-        pnts = np.linspace(xmin, xmax, num=(n+1))
-        lenx = abs(xmax - xmin)
-        delta = np.diff(pnts)
-    
-        verts = pnts[:-1]
-        edges = (pnts[:-1], pnts[1:])
-    
-        verts_dual = verts + 0.5*delta
-        edges_dual = (np.roll(verts_dual,shift=1), verts_dual)
-        edges_dual[0][0] -= lenx
-        delta_dual = delta
-    
-        B0  = lambda i, x: sp.phi0(n, i, x)
-        B1  = lambda i, x: sp.phi1(n, i, x)
-        B0d = lambda i, x: sp.phid0(n, i, x)
-        B1d = lambda i, x: sp.phid1(n, i, x)
-        
-        P0,  P1  = cm.projections((verts, edges))
-        P0d, P1d = cm.projections((verts_dual, edges_dual))
-        
-        D0  = lambda f: np.roll(f, shift=-1) - f
-        D1  = lambda f: 0 
-        D0d = lambda f: np.roll(D0(f), shift=+1)
-        D1d = lambda f: 0 
-
-        H0 = lambda x: np.real(sp.H(x))
-        H1 = lambda x: np.real(sp.Hinv(x))
-        H0d = H0
-        H1d = H1
-        
-        d = cls(n=n,
-                xmin=xmin,
-                xmax=xmax,
-                pnts=pnts,
-                delta=delta_dual, 
-                N = (n, n),
-                simp=(verts_dual, edges_dual),
-                dec=cm.dec_operators(P=(P0d, P1d),
-                                     B=(B0d, B1d),
-                                     D=(D0d, D1d),
-                                     H=(H0d, H1d)),
-                dual=None)
-        
-        g = cls(n=n,
-                xmin=xmin, 
-                xmax=xmax,
-                pnts=pnts, 
-                delta=delta,
-                N = (n, n),
-                simp=(verts, edges),
-                dec=cm.dec_operators(P=(P0, P1),
-                                     B=(B0, B1),
-                                     D=(D0, D1),
-                                     H=(H0, H1)),
-                dual=None)  
-    
-        g.dual, d.dual = d, g
-    
-        return g
+Grid_1D.periodic = classmethod(periodic_fn)
 
 class Grid_1D_Periodic:
 
